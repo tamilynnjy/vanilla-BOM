f53f55dadd328b62ee86bfdb0c98aa8e93d53bc4
tamilyn <tamilynnjy@gmail.com>
2024-10-02 16:57:48 +1000

    basic forecast added
    HTML from Javascript

--- src/index.js
+++ src/index.js
@@ -56,7 +56,31 @@ function getWeather(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15°C</strong>
+              </div>
+              <div class="weather-forecast-temperature">9°C</div>
+            </div>
+          </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let form = document.querySelector("form");
 form.addEventListener("submit", displayCity);
 
 getWeather("Paris");
+displayForecast();
